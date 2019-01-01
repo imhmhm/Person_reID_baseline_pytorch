422ebff84ce59bcf0d409b4fd1ae7f89571c201f
Hengheng Zhang <imhmhm@gmail.com>
2019-01-01 13:29:34 -0600

    init for gen

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python test.py \
-       --name ft_ResNet50_1toM_s0.1_p0.5 \
+       --name ft_ResNet50_1toM_s0.5_p0.5_rm \
        --test_dir /home/tianlab/hengheng/reid/Market/pytorch_1toM \
        --which_epoch 59 \
        #--name ft_ResNet50_1toM_s0.1_p0.5 \
--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 python train.py \
-       --name ft_ResNet50_1toM_s0.1_p0.5 \
+       --name ft_ResNet50_1toM_s0.5_p0.5_rm \
        --data_dir /home/tianlab/hengheng/reid/Market/pytorch_1toM \
        --train_all \
        #--name ft_ResNet50_128_relu \
