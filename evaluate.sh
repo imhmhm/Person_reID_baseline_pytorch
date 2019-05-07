70ba0feccabb649596e8f67a6a85285eb9b2c81a
imhmhm <imhmhm@gmail.com>
2019-05-07 15:41:36 -0500

    beforeMerge

--- evaluate.sh
+++ evaluate.sh
@@ -1,31 +1,31 @@
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set Market \
 --which_epoch 59;
 
 # DukeMTMC-reID / Market
 
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set Market \
 --which_epoch 99;
 
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set Market \
 --which_epoch 119;
 
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set DukeMTMC-reID \
 --which_epoch 59;
 
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set DukeMTMC-reID \
 --which_epoch 99;
 
 python evaluate.py \
---name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
 --test_set DukeMTMC-reID \
 --which_epoch 119;
--- train.sh
+++ train.sh
@@ -19,6 +19,17 @@ python train.py \
 # --PCB \
 # --data_dir /home/hmhm/reid/DukeMTMC-reID/pytorch \
 
+python train.py \
+--name ft_ResNet50_BT_b32x4_adam_tri_cudnnBenchmark \
+--data_dir /home/tianlab/hengheng/reid/Market/pytorch \
+--train_all \
+--batchsize 128 \
+--droprate 0 \
+--use_sampler \
+--num_per_id 4 \
+--triplet \
+--stride 2 \
+--adam;
 
 # python train.py \
 # --name ft_ResNet50_BT_b16x4_sgd0.01_[30_70]_tri \
