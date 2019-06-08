ce740f8e828c71d58b2db515c57bd64177e2f368
imhmhm <imhmhm@gmail.com>
2019-06-08 14:40:58 -0500

    stitch_metric

--- evaluate.sh
+++ evaluate.sh
@@ -1,31 +1,31 @@
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set Market \
 --which_epoch 59;
 
 # # DukeMTMC-reID / Market
 
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set Market \
 --which_epoch 99;
 
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set Market \
 --which_epoch 119;
 
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set DukeMTMC-reID \
 --which_epoch 59;
 
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set DukeMTMC-reID \
 --which_epoch 99;
 
 python evaluate.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_set DukeMTMC-reID \
 --which_epoch 119;
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set Market \
 --which_epoch 59;
@@ -10,31 +10,31 @@ python test.py \
 #--which_epoch \
 
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set Market \
 --which_epoch 99;
 
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set Market \
 --which_epoch 119;
 
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set DukeMTMC-reID \
 --which_epoch 59;
 
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set DukeMTMC-reID \
 --which_epoch 99;
 
 python test.py \
---name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_easyPos \
+--name ft_ResNet50_b16x4_adam_stitch_metric_test_v2_margin_all_0.3 \
 --test_dir /home/tianlab/hengheng/reid \
 --test_set DukeMTMC-reID \
 --which_epoch 119;
